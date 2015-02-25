24d400083cccbe89f197ef7365735b8b34a6dbb7
Elie Schoppik <eschoppik@gmail.com>
2015-02-25 08:58:39 -0800

    cleaning up folder structure

---
+++ 3 - back_to_college_solution/app/models/student.rb
@@ -0,0 +1,18 @@
+class Student < ActiveRecord::Base
+
+  has_many :courses, through: :enrollments
+  has_many :enrollments
+
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    length: {minimum: 3}
+
+  def enroll ids
+    enrollments.where("course_id NOT IN (?)", ids.map(&:to_i)).delete_all
+    ids.reject(&:empty?).each do |id|
+      enrollments.find_or_create_by(course_id: id)
+    end
+  end
+
+end
